6c21c2dfa47065f92f1252d9fe05fa93d81182a7
matt burch <matt.burch@optiv.com>
2018-12-12 09:37:19 -0500

    Syntax Corrections

--- kismet.go
+++ kismet.go
@@ -4,109 +4,114 @@ package kismet
 
 import (
 	"encoding/xml"
+	"golang.org/x/net/html/charset"
+	"io"
 )
 
 type KismetRun struct {
-	Version    string     `json:"kismet-version,attr"`
-	StartTime  string     `json:"start-time,attr"`
-	CardSource CardSource `json:"card-source"`
-	Networks   []Network  `json:"wireless-network"`
+	Version    string     `xml:"kismet-version,attr"`
+	StartTime  string     `xml:"start-time,attr"`
+	CardSource CardSource `xml:"card-source"`
+	Networks   []Network  `xml:"wireless-network"`
 }
 
 type CardSource struct {
-	UUID     string `json:"uuid,attr"`
-	Source   string `json:"card-source"`
-	Name     string `json:"card-name"`
-	Type     string `json:"card-type"`
-	Packets  int    `json:"card-packets"`
-	Hop      bool   `json:"card-hop"`
-	Channels string `json:"card-channels"`
+	UUID     string `xml:"uuid,attr"`
+	Source   string `xml:"card-source"`
+	Name     string `xml:"card-name"`
+	Type     string `xml:"card-type"`
+	Packets  int    `xml:"card-packets"`
+	Hop      bool   `xml:"card-hop"`
+	Channels string `xml:"card-channels"`
 }
 
 type Network struct {
-	Number       string    `json:"number,attr"`
-	Type         string    `json:"type,attr"`
-	FirstTime    string    `json:"first-time,attr"`
-	LastTime     string    `json:"last-time,attr"`
-	SSID         SSID      `json:"SSID"`
-	BSSID        string    `json:"BSSID"`
-	Manufacturer string    `json:"manuf"`
-	Channel      int       `json:"channel"`
-	MHZ          string    `json:"freqmhz"`
-	MaxSeen      int       `json:"maxseenrate"`
-	Packets      Packets   `json:"packets"`
-	DataSize     int       `json:"datasize"`
-	SNRInfo      SNRInfo   `json:"snr-info"`
-	BSSTimeStamp string    `json:"bsstimestamp"`
-	CDPDevice    string    `json:"cdp-device"`
-	CDPPortID    string    `json:"cdp-portid"`
-	SeenCard     SeenCard  `json:"seen-card"`
-	Clients      []Clients `json:"wireless-clients"`
+	Number       string    `xml:"number,attr"`
+	Type         string    `xml:"type,attr"`
+	FirstTime    string    `xml:"first-time,attr"`
+	LastTime     string    `xml:"last-time,attr"`
+	SSID         SSID      `xml:"SSID"`
+	BSSID        string    `xml:"BSSID"`
+	Manufacturer string    `xml:"manuf"`
+	Channel      int       `xml:"channel"`
+	MHZ          string    `xml:"freqmhz"`
+	MaxSeen      int       `xml:"maxseenrate"`
+	Packets      Packets   `xml:"packets"`
+	DataSize     int       `xml:"datasize"`
+	SNRInfo      SNRInfo   `xml:"snr-info"`
+	BSSTimeStamp string    `xml:"bsstimestamp"`
+	CDPDevice    string    `xml:"cdp-device"`
+	CDPPortID    string    `xml:"cdp-portid"`
+	SeenCard     SeenCard  `xml:"seen-card"`
+	Clients      []Clients `xml:"wireless-clients"`
 }
 
 type SSID struct {
-	FirstTime  string   `json:"first-time,attr"`
-	LastTime   string   `json:"last-time,attr"`
-	Type       string   `json:"type"`
-	MaxRate    string   `json:"max-rate"`
-	Packets    int      `json:"packets"`
-	Encryption []string `json:"encryption"`
-	ESSID      ESSID    `json:"essid"`
+	FirstTime  string   `xml:"first-time,attr"`
+	LastTime   string   `xml:"last-time,attr"`
+	Type       string   `xml:"type"`
+	MaxRate    string   `xml:"max-rate"`
+	Packets    int      `xml:"packets"`
+	Encryption []string `xml:"encryption"`
+	ESSID      ESSID    `xml:"essid"`
 }
 
 type ESSID struct {
-	Cloaked bool   `json:"cloaked,attr"`
-	ESSID   string `json:"essid"`
+	Cloaked bool   `xml:"cloaked,attr"`
+	ESSID   string `xml:"essid"`
 }
 
 type Packets struct {
-	LLC       int `json:"LLC"`
-	Data      int `json:"data"`
-	Crypt     int `json:"crypt"`
-	Total     int `json:"total"`
-	Fragments int `json:"fragments"`
-	Retries   int `json:"retries"`
+	LLC       int `xml:"LLC"`
+	Data      int `xml:"data"`
+	Crypt     int `xml:"crypt"`
+	Total     int `xml:"total"`
+	Fragments int `xml:"fragments"`
+	Retries   int `xml:"retries"`
 }
 
 type SNRInfo struct {
-	LastSigDBM    uint `json:"last_signal_dbm"`
-	LastNoiseDBM  uint `json:"last_noise_dbm"`
-	LastSigRSSI   uint `json:"last_signal_rssi"`
-	LastNoiseRSSI uint `json:"last_noise_rssi"`
-	MinSigDBM     uint `json:"min_signal_dbm"`
-	MinNoiseDBM   uint `json:"min_noise_dbm"`
-	MinSignalRSSI uint `json:"min_signal_rssi"`
-	MinNoiseRSSI  uint `json:"min_noise_rssi"`
-	MaxSigDBM     uint `json:"max_signal_dbm"`
-	MaxNoiseDBM   uint `json:"max_noise_dbm"`
-	MaxSigRSSI    uint `json:"max_signal_rssi"`
-	MaxNoiseRSSI  uint `json:"max_noise_rssi"`
+	LastSigDBM    int `xml:"last_signal_dbm"`
+	LastNoiseDBM  int `xml:"last_noise_dbm"`
+	LastSigRSSI   int `xml:"last_signal_rssi"`
+	LastNoiseRSSI int `xml:"last_noise_rssi"`
+	MinSigDBM     int `xml:"min_signal_dbm"`
+	MinNoiseDBM   int `xml:"min_noise_dbm"`
+	MinSignalRSSI int `xml:"min_signal_rssi"`
+	MinNoiseRSSI  int `xml:"min_noise_rssi"`
+	MaxSigDBM     int `xml:"max_signal_dbm"`
+	MaxNoiseDBM   int `xml:"max_noise_dbm"`
+	MaxSigRSSI    int `xml:"max_signal_rssi"`
+	MaxNoiseRSSI  int `xml:"max_noise_rssi"`
 }
 
 type SeenCard struct {
-	SeenUUID    string `json:"seen-uuid"`
-	SeenTime    string `json:"seen-time"`
-	SeenPackets int    `json:"seen-packets"`
+	SeenUUID    string `xml:"seen-uuid"`
+	SeenTime    string `xml:"seen-time"`
+	SeenPackets int    `xml:"seen-packets"`
 }
 
 type Clients struct {
-	ClientMAC   string   `json:"client-mac"`
-	ClientManuf string   `json:"client-manuf"`
-	Channel     int      `json:"channel"`
-	FreqMHZ     string   `json:"freqmhz"`
-	MaxSeenRate int      `json:"maxseenrate"`
-	Packets     Packets  `json:"packets"`
-	DataSize    int      `json:"datasize"`
-	SNRInfo     SNRInfo  `json:"snr-info"`
-	SeenCard    SeenCard `json:"snr-info"`
+	ClientMAC   string   `xml:"client-mac"`
+	ClientManuf string   `xml:"client-manuf"`
+	Channel     int      `xml:"channel"`
+	FreqMHZ     string   `xml:"freqmhz"`
+	MaxSeenRate int      `xml:"maxseenrate"`
+	Packets     Packets  `xml:"packets"`
+	DataSize    int      `xml:"datasize"`
+	SNRInfo     SNRInfo  `xml:"snr-info"`
+	SeenCard    SeenCard `xml:"snr-info"`
 }
 
 // Parse takes a byte array of sslscan xml data and unmarshals it into an
 // KismetRun struct. All elements are returned as strings, it is up to the caller
 // to check and cast them to the proper type.
-func Parse(content []byte) (*KismetRun, error) {
+func Parse(content io.Reader) (*KismetRun, error) {
 	r := &KismetRun{}
-	err := xml.Unmarshal(content, r)
+	// Convert Charset to UTF-8
+	decoder := xml.NewDecoder(content)
+	decoder.CharsetReader = charset.NewReaderLabel
+	err := decoder.Decode(&r)
 	if err != nil {
 		return r, err
 	}
